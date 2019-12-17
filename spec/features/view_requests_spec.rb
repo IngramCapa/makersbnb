156f5c9fb2a18c4ee4bf68b7b60ffab10ebd580d
bensheridanedwards <bensheridanedwards@gmail.com>
2019-12-17 14:48:14 +0000

    Modify test routes, all tests passing

--- spec/features/view_requests_spec.rb
+++ spec/features/view_requests_spec.rb
@@ -1,24 +1,24 @@
 feature 'View Requests' do 
   scenario 'a user should be able to see the request columns' do
-    visit('/')
+    visit('/requests')
     expect(page).to have_content "Requests Made"
     expect(page).to have_content "Requests Received"
   end
 
   scenario 'a user should be able to see their booking requests' do
-    visit('/')
+    visit('/requests')
     expect(page).to have_content 'Booking 1'
     expect(page).to have_content 'Booking 2'
     expect(page).to have_content 'Booking 3'
   end
 
   scenario 'a user should be able to see if their booking is confirmed' do
-    visit('/')
+    visit('/requests')
     expect(page).to have_content 'Confirmed'
   end
 
   scenario 'a user should be able to the date the booking was submitted' do
-    visit('/')
+    visit('/requests')
     expect(page).to have_content 'Date Booked:'
   end
 end
