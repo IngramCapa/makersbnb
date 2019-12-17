5a58f84d3f59811cb2f603fd28b669b3977ab69a
Raluca Ciucu <raluca.ciucu@gmail.com>
2019-12-17 10:27:53 +0000

    Add migrations tables with examples

--- db/migrations/create_properties_table.sql
+++ db/migrations/create_properties_table.sql
@@ -3,8 +3,14 @@ CREATE TABLE properties(
   prop_name VARCHAR(200),
   prop_description VARCHAR(2000),
   price_per_night INTEGER,
-  startdate date(yyyy-mm-dd),
-  enddate date(dd/mm/yyyy),
+  startdate DATE,
+  enddate DATE,
   owner_id INTEGER REFERENCES users(id)
-    --   ts TIMESTAMP, maybe for sorting desc thru listings
+    
 );
+
+-- example:
+
+-- INSERT INTO properties(prop_name, prop_description, price_per_night, startdate, enddate)
+-- VALUES
+-- 	('makers house', 'old building in aldgate east', 20, '2019-12-17', '2019-12-31');
--- db/migrations/create_users_table.sql
+++ db/migrations/create_users_table.sql
@@ -4,3 +4,9 @@ CREATE TABLE users(
   email TEXT NOT NULL unique,
   password_digest TEXT NOT NULL
 );
+
+-- example:
+
+-- INSERT INTO users(user_name, email, password_digest)
+-- VALUES
+--    ('name3', 'name3@mail.com', 'test');
