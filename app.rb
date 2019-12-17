e5f0ea5f1cd1b87e9ebe8aaa18b3922e600b7de7
bensheridanedwards <bensheridanedwards@gmail.com>
2019-12-17 14:20:56 +0000

    Add routes to app file

--- app.rb
+++ app.rb
@@ -3,10 +3,26 @@ require 'sinatra/base'
 class MakersAirBnB < Sinatra::base
 
   get '/' do
-
   end
 
-  
+  get '/sessions/new'
+  end
+
+  get '/spaces' do 
+  end
+
+  get '/spaces/new'
+  end
+
+  get '/spaces/dates'
+  end
+
+  get '/requests'
+  erb :'requests'
+  end
+
+  get '/requests/confirm'
+  end
 
   run! if app_file == $0
 end
