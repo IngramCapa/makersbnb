f4b999dc6e19b2dc59abd52a79bea52c8349bfd9
bensheridanedwards <bensheridanedwards@gmail.com>
2019-12-19 12:31:08 +0000

    Update all files

--- app.rb
+++ app.rb
@@ -2,9 +2,14 @@ require 'sinatra/base'
 require_relative './lib/bookings.rb'
 require_relative './lib/requests.rb'
 
+
 class MakersAirBnB < Sinatra::Base
 
   get '/' do
+    @bookings = Bookings.new
+    @user_id = 1
+    @requests = Requests.new(@user_id)
+    erb :dick
   end
 
   post '/' do
